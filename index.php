721e7e85e8db2591345b03f883aaae5ad1b83461
tevenfeng <fengdingwen@outlook.com>
2015-11-05 19:43:01 +0800

    在线分词调用功能初步完成

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tevenfeng
+ * Date: 15-11-5
+ * Time: 下午4:59
+ */
+
+function send_post($url, $post_data)
+{
+    $postData = http_build_query($post_data);
+    $options = array(
+        'http' => array(
+            'method' => 'POST',
+            'content' => $postData,
+            'timeout' => 15 * 60
+        )
+    );
+    $context = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+
+    $de_json = json_decode($result);
+
+    $status = $de_json->status;
+    $message = $de_json->message;
+    $words = $de_json->words;
+
+    foreach ($words as $word) {
+        echo $word->word;
+    }
+}
+
+$dataToPost = array(
+    'data' => '这是一个测试的句子，用来测试在线分词功能。',
+    'respond' => 'json',
+    'ignore' => 'true'
+);
+
+send_post('http://www.xunsearch.com/scws/api.php', $dataToPost);
