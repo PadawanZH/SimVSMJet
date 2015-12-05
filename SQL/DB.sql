4fb1d51116367584f0ee59782a08c5bcfd7eca7e
tevenfeng <fengdingwen@outlook.com>
2015-12-05 21:24:36 +0800

    ER图

--- SQL/DB.sql
+++ SQL/DB.sql
@@ -10,20 +10,50 @@ CREATE TABLE IF NOT EXISTS document
     WORDCOUNT			LONG
 );
 
-CREATE TABLE IF NOT EXISTS term
+CREATE TABLE IF NOT EXISTS titleTerms
 (
-	CONTENT				VARCHAR(255) PRIMARY KEY,
+	WORD				VARCHAR(255) PRIMARY KEY,
     IDF					DOUBLE,
     LENGTH				INT
 );
 
-CREATE TABLE IF NOT EXISTS doc_term
+CREATE TABLE IF NOT EXISTS doc_titleTerms
 (
 	ID					INT,
-    CONTENT				VARCHAR(255),
+    WORD				VARCHAR(255),
     TF					INT,
     OFFSET				LONG,
-    PRIMARY KEY(ID,CONTENT,TF),
+    PRIMARY KEY(ID,WORD,TF),
     CONSTRAINT FOREIGN KEY (ID) REFERENCES document(ID),
-    CONSTRAINT FOREIGN KEY (CONTENT) REFERENCES term(CONTENT)
-)
+    CONSTRAINT FOREIGN KEY (WORD) REFERENCES titleTerms(WORD)
+);
+
+CREATE TABLE IF NOT EXISTS contentTerms
+(
+	WORD				VARCHAR(255) PRIMARY KEY,
+    IDF					DOUBLE,
+    LENGTH				INT
+);
+
+CREATE TABLE IF NOT EXISTS doc_contentTerms
+(
+	ID					INT,
+    WORD				VARCHAR(255),
+    TF					INT,
+    OFFSET				LONG,
+    PRIMARY KEY(ID,WORD,TF),
+    CONSTRAINT FOREIGN KEY (ID) REFERENCES document(ID),
+    CONSTRAINT FOREIGN KEY (WORD) REFERENCES contentTerms(WORD)
+);
+
+load data infile "/home/tevenfeng/Coding/php/TxtIntoSql/document.txt" IGNORE into table document fields terminated by "," lines terminated by "\n"; 
+load data infile "/home/tevenfeng/Coding/php/TxtIntoSql/titleTerms.txt" IGNORE into table titleTerms fields terminated by "," lines terminated by "\n";
+load data infile "/home/tevenfeng/Coding/php/TxtIntoSql/contentTerms.txt" IGNORE into table contentTerms fields terminated by "," lines terminated by "\n";
+load data infile "/home/tevenfeng/Coding/php/TxtIntoSql/doc_titleTerms.txt" IGNORE into table doc_titleTerms fields terminated by "," lines terminated by "\n";
+load data infile "/home/tevenfeng/Coding/php/TxtIntoSql/doc_contentTerms.txt" IGNORE into table doc_contentTerms fields terminated by "," lines terminated by "\n";
+
+select * from document;
+select * from titleTerms;
+select * from contentTerms;
+select * from doc_titleTerms;
+select * from doc_contentTerms;
